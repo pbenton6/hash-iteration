edc52bf7bf441ecb6da00a586dcea881a8b143f4
pbenton6 <patrick.benton6396@gmail.com>
2021-01-20 15:02:26 -0600

    commit tree 6e1e08368ae30d3d5796fcfb533cfe414e6f82aa

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -6,6 +6,13 @@
 # suite_e: "Crumpet the Elf"
 # }
 
+
 def select_winner(passengers)
-  # add the code snippet here!
+  winner = ""
+  passengers.each do |suite, name|
+    if suite == :suite_a && name.start_with?("A")
+      winner = name
+    end
+    return winner
+  end
 end
